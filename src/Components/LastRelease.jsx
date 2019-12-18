1f4e5f7ffef45c10935c5f2cc115262a6f79b90d
boubaSambare <boubasambare@yahoo.it>
2019-12-18 01:34:27 +0100

    delete file

--- src/Components/LastRelease.jsx
+++ src/Components/LastRelease.jsx
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
-import Fantasy from "../data/fantasy.json";
-import History from "../data/history.json";
-import Horror from "../data/horror.json";
-import Romance from "../data/romance.json";
-import Scifi from "../data/scifi.json";
 import LibraryCard from './LibraryCard'
 import SearchComponent from "./SearchComponent.jsx";
 import SingleBook from "./SingleBook.jsx";
